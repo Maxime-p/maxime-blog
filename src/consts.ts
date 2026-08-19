1444a67dee9abcc57cb9d7dee3a7302feaa1dbb3
Maxime Perraud <contact@maxime-p.dev>
2026-08-19 22:09:43 +0200

    feat(links): replace hardcoded LinkedIn and GitHub URLs with constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const SITE_TITLE = 'Maxime-p.dev';
 export const SITE_DESCRIPTION =
 	'Articles and curated JavaScript development resources from Maxime.';
+export const LINKEDIN_URL = 'http://www.linkedin.com/in/maxime-perraud/';
+export const GITHUB_URL = 'https://github.com/Maxime-p';
